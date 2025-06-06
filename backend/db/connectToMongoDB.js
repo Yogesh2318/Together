77e4e59004e2a188ed8a6aa9c3bc2cd8586c1e71
Yogesh vishwanath Bhivsane <158162127+Yogesh2318@users.noreply.github.com>
2025-06-06 20:31:27 +0530

    Update connectToMongoDB.js
    minor change

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -4,7 +4,7 @@ dotenv.config();
 const connectToMongoDB = async () => {
     try {
     
-        await mongoose.connect(process.env.MONGO_URL||"mongodb+srv://yvbhivasane:YXviYAYUiJgX33Im@cluster0.hbecw.mongodb.net/Together?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect(process.env.MONGO_URL||"")
         console.log("connected to MONGO-DB");
     } catch (error) {
         console.log("Error connecting  to MongoDB ",error.message)
